014336411e68cd5c87b338ae36d19ced882c59fe
larpios <larpios@protonmail.com>
2023-12-08 02:03:06 +0900

    Update Profile

--- Files/Microsoft.PowerShell_profile.ps1
+++ Files/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,12 @@
 oh-my-posh init pwsh --config "C:\Program Files (x86)\oh-my-posh\themes\kushal.omp.json" | Invoke-Expression
 
-Set-Alias -Name vim -Value "C:\Program Files\Neovim\bin\nvim.exe"
+if (Test-Path "C:\Program Files\Neovim\bin\nvim.exe")
+{
+
+    Set-Alias -Name vim -Value "C:\Program Files\Neovim\bin\nvim.exe"
+} else
+{
+    Set-Alias -Name vim -Value "C:\tools\Neovim\bin\nvim.exe"
+}
+
 
