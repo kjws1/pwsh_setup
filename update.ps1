67cc7eb0dff1fe842333ad8ff80c9a123737e6ad
larpios <larpios@protonmail.com>
2023-12-08 02:02:06 +0900

    refactor

--- update.ps1
+++ update.ps1
@@ -1,2 +1,3 @@
 Copy-Item -Path $HOME/.wezterm.lua -Destination ./Files -Force
-Copy-Item -Path $PROFILE.CurrentUserCurrentHost -Destination ./Files -Force
+Copy-Item -Path $PROFILE -Destination ./Files -Force
+
